e0f8b695e6c6fac90594da3d2d1305db1f48e58f
markus <markus@markus-SATELLITE-L855>
2013-11-01 14:23:31 +0100

    Can take an input and show an output Can put the input into the output Can clear the output

---
+++ ChaosPoems/src/chaosPoems/Main.java
@@ -0,0 +1,12 @@
+package chaosPoems;
+
+public class Main
+{
+
+	public static void main(String[] args)
+	{
+		Window w = new Window();
+		w.drawWindow();
+	}
+
+}
---
+++ ChaosPoems/src/chaosPoems/Window.java
@@ -0,0 +1,69 @@
+package chaosPoems;
+
+import java.awt.BorderLayout;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
+import javax.swing.SwingConstants;
+
+public class Window
+{
+	JFrame frame;
+	JTextArea input;
+	JLabel output;
+	JPanel buttons;
+	JButton generate;
+	JButton clear;
+	
+	public Window()
+	{
+		
+	}
+	
+	public void drawWindow()
+	{
+		frame = new JFrame("Chaos Poem");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setLayout(new GridLayout(3, 1));
+		input = new JTextArea(25, 50);
+		frame.getContentPane().add(input);
+		addButtons();
+		output = new JLabel();
+		frame.getContentPane().add(output);
+		frame.pack();
+		frame.setVisible(true);
+	}
+	
+	private void addButtons()
+	{
+		buttons = new JPanel();
+		buttons.setLayout(new GridLayout(1, 2));
+		generate = new JButton("Generate");
+		generate.addActionListener(new
+			ActionListener() 
+			{
+				public void actionPerformed(ActionEvent e)
+				{
+					output.setText(input.getText());
+				}
+			});
+		clear = new JButton("Clear");
+		clear.addActionListener(new
+			ActionListener()
+			{
+				public void actionPerformed(ActionEvent e)
+				{
+					output.setText("");
+				}
+			});
+		buttons.add(generate);
+		buttons.add(clear);
+		frame.getContentPane().add(buttons);
+	}
+}
