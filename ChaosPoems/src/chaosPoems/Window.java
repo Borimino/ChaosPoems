a25831feb10db4b94d52fd0046041133be941ab5
markus <markus@markus-SATELLITE-L855>
2013-11-01 14:36:31 +0100

    Nothing

--- ChaosPoems/src/chaosPoems/Window.java
+++ ChaosPoems/src/chaosPoems/Window.java
@@ -1,6 +1,5 @@
 package chaosPoems;
 
-import java.awt.BorderLayout;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -10,7 +9,6 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextArea;
-import javax.swing.SwingConstants;
 
 public class Window
 {
